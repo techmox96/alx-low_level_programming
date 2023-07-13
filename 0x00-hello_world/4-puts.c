e4154bca34c5c8562c22cf71b2925e8199569a31
techmox <aminehakim69@gmail.com>
2023-07-13 12:26:44 +0000

    C - Hello, World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
-#include <stdio.h>
-
+#include<stdio.h>
 /**
  *  * main - Entry point
  *   *
@@ -7,6 +6,6 @@
  */
 int main(void)
 {
-		printf("with proper grammar, but the outcome is a piece of art,\n");
+		puts("\"Programming is like building a multilingual puzzle");
 			return (0);
 }
