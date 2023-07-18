7f9fd60f4c9fc5370096868ad481f429ef7791b2
techmox <aminehakim69@gmail.com>
2023-07-18 15:07:21 +0000

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,19 @@
 #include "main.h"
 /**
- * main - Entry point
- * Descreptiop : printing _putchar, followed by a new line.'
- * Return: 0
+ * main -print_putchar
+ * Return : 0
  */
-int main(void) {
-	    _putchar('_');
-	    _putchar('p');
-	    _putchar('u');
-	    _putchar('t');
-	    _putchar('c');
-	    _putchar('h');
-	    _putchar('a');
-	    _putchar('r');
-	    _putchar('\n');
-return (0);
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
