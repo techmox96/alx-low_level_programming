2e33eb4e8eba49f3a85128d744e9a3488cfcdd4c
techmox <aminehakim69@gmail.com>
2023-07-18 10:39:52 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,18 +6,20 @@
  */
 void print_alphabet_x10(void)
 {
-		char ch;
-		int i;
-		i = 0;
-		while (i < 10)
+	char ch;
+	int i;
+
+	i = 0;
+
+	while (i < 10)
+	{
+		ch = 'a';
+		while (ch <= 'z')
 		{
-			ch = 'a';
-			while (ch <= 'z')
-			{
-				_putchar(ch);
-				ch++;
-			}
-			_putchat('\n');
-			i++;
+			_putchar(ch);
+			ch++;
 		}
+		_putchar('\n');
+		i++;
+	}
 }
