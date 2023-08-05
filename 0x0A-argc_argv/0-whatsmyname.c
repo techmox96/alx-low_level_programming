0a2971218ddc2ad7ac91046311992ec3b6f58fa1
techmox <aminehakim69@gmail.com>
2023-08-05 14:41:44 +0000

    1

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,9 +9,9 @@ main - prints the name of the program
 *
 Return: Always 0 (Success)
 */
-int main(int argc attribute((unused)), char argv[])
+int main(int argc attribute((unused)), char** argv)
 {
-    printf("%s\n",argv);
+    printf("%s\n",argv [0]);
 
     return (0);
 }
