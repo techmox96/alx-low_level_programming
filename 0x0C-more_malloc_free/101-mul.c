75512f6bb35038de5eb412179383795c42e1a8e3
techmox <aminehakim69@gmail.com>
2023-08-10 13:18:57 +0000

    test

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -91,11 +91,11 @@ int main(int argc, char *argv[])
 		if (result[i])
 			a = 1;
 		if (a)
-			putchar(result[i] + '0'); // Changed from _putchar(result[i] + '0');
+			_putchar(result[i] + '0');
 	}
 	if (!a)
-		putchar('0'); // Changed from _putchar('0');
-	putchar('\n'); // Changed from _putchar('\n');
+		_putchar('0');
+	_putchar('\n');
 	free(result);
 	return (0);
 }
