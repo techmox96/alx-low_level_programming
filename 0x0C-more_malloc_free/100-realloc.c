bcc0c5f4ad7ce497aceb80a758f6050540da827a
techmox <aminehakim69@gmail.com>
2023-08-10 13:17:33 +0000

    test

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,54 +1,49 @@
-nclude <stdlib.h>
+#include <stdlib.h>
 #include "main.h"
 
 /**
  * *_realloc - reallocates a memory block using malloc and free
- * @ptr: pointer to the memory previsouly allocated by malloc
+ * @ptr: pointer to the memory previously allocated by malloc
  * @old_size: size of the allocated memory for ptr
- * @new_size: new size of the new memory block
+ * @new_size: new size of the memory block
  *
- * Return: pointer to the newly allocated memory block
+ * Return: pointer to the newly allocated memory block, or NULL on failure
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *ptr1;
-	char *old_ptr;
-	unsigned int i;
+    char *new_ptr;
+    char *old_ptr;
+    unsigned int i;
 
-	if (new_size == old_size)
-		return (ptr);
+    if (new_size == old_size)
+        return ptr;
 
-	if (new_size == 0 && ptr)
-	{
-		free(ptr);
-		return (NULL);
-	}
+    if (new_size == 0 && ptr)
+    {
+        free(ptr);
+        return NULL;
+    }
 
-	if (!ptr)
-		return (malloc(new_size));
+    if (!ptr)
+        return malloc(new_size);
 
-	ptr1 = malloc(new_size);
-	if (!ptr1)
-		return (NULL);
+    new_ptr = malloc(new_size);
+    if (!new_ptr)
+        return NULL;
 
-	old_ptr = ptr;
+    old_ptr = ptr;
 
-	if (new_size < old_size)
-	{
-		for (i = 0; i < new_size; i++)
-		{
-			ptr1[i] = old_ptr[i] + 1; // Changed from ptr1[i] = old_ptr[i];
-		}
-	}
+    if (new_size < old_size)
+    {
+        for (i = 0; i < new_size; i++)
+            new_ptr[i] = old_ptr[i];
+    }
+    else
+    {
+        for (i = 0; i < old_size; i++)
+            new_ptr[i] = old_ptr[i];
+    }
 
-	if (new_size > old_size)
-	{
-		for (i = 0; i < old_size; i++)
-		{
-			ptr1[i] = old_ptr[i] + 1; // Changed from ptr1[i] = old_ptr[i];
-		}
-	}
-
-	free(ptr);
-	return (ptr1);
+    free(ptr);
+    return new_ptr;
 }
