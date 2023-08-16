0e8b39d73c2742626c114f7abd31d58a42aa106f
techmox <aminehakim69@gmail.com>
2023-08-16 10:09:20 +0000

    0x0F-function_pointers

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,33 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void print_opcodes(const char *start, size_t length) {
-    for (size_t i = 0; i < length; i++) {
-        printf("%02hhx", start[i]);
-        if (i < length - 1) {
-            printf(" ");
-        }
-    }
-    printf("\n");
-}
+/**
+ * main - prints its own opcodes
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int bytes, i;
+	char *arr;
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        printf("Error\n");
-        return 1;
-    }
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
 
-    int num_bytes = atoi(argv[1]);
+	bytes = atoi(argv[1]);
 
-    if (num_bytes <= 0) {
-        printf("Error\n");
-        return 2;
-    }
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
 
-    // Get the starting address of the main function
-    char *main_address = (char *)main;
+	arr = (char *)main;
 
-    print_opcodes(main_address, num_bytes);
-
-    return 0;
+	for (i = 0; i < bytes; i++)
+	{
+		if (i == bytes - 1)
+		{
+			printf("%02hhx\n", arr[i]);
+			break;
+		}
+		printf("%02hhx ", arr[i]);
+	}
+	return (0);
 }
